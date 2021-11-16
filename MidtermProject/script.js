98853d49addb27f2f0df8ffcaef3de83fb15e152
liga6085 <70085102+liga6085@users.noreply.github.com>
2021-11-15 19:46:46 -0700

    Update script.js

--- MidtermProject/script.js
+++ MidtermProject/script.js
@@ -33,3 +33,11 @@
 //   modal.classList.remove('active')
 //   overlay.classList.remove('active')
 // }
+
+const toggleModal = () => {
+  document.querySelector('.modal')
+    .classList.toggle('modal--hidden');
+};
+
+document.querySelector('#show-modal')
+  .addEventListener('click', toggleModal);
