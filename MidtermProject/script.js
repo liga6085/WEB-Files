cfb5d59b07a9e50f9a8368acd52cc105fc8a620c
liga6085 <70085102+liga6085@users.noreply.github.com>
2021-11-18 17:46:53 -0700

    Update script.js

--- MidtermProject/script.js
+++ MidtermProject/script.js
@@ -1,48 +1,50 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]');
-const closeModalButtons = document.querySelectorAll('[data-close-button');
-const overlay = document.getElementById('overlay');
+// const openModalButtons = document.querySelectorAll('[data-modal-target]');
+// const closeModalButtons = document.querySelectorAll('[data-close-button');
+// const overlay = document.getElementById('overlay');
+
+let hexagon = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 
 
 for (i=0; i<document.getElementsByClassName('hexagon').length;i++){
  console.log(document.getElementsByClassName('hexagon')[i].id);
- document.getElementsByClassName('hexagon')[i].addEventListener("click", open);
+ document.getElementsByClassName('hexagon')[i].addEventListener("click", open, false);
+ document.getElementsByClassName('hexagon')[i].id = hexagon[i];
 }
 
 function open(e){
   console.log(e.target.id);
-  document.getElementById('modal').classList.toggle('active');
+  // document.getElementById('su-modal').classList.toggle('active');
   document.getElementById('overlay').classList.toggle('active');
   document.getElementById('overlay').addEventListener('click', close);
+
+  if(e.target.tagName == "H1"){
+    console.log("h1 clicked");
+    e.target.classList.toggle('active');
+  }
+  else{
+    console.log("div clicked");
+    e.target.classList.toggle('active');
+  }
 }
 
 function close(e){
   if(e.target.id == "overlay" || e.target.id == "close"){
-  document.getElementById('modal').classList.toggle('active');
+  document.getElementById('su-modal').classList.toggle('active');
   document.getElementById('overlay').classList.toggle('active');
 }
 }
-// openModalButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     const modal = document.querySelector(button.dataset.modalTarget)
-//     openModal(modal);
-//   })
-// })
-//
-// closeModalButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     const modal = button.closest('.modal');
-//     closeModal(modal);
-//   })
-// })
-//
-// function openModal(modal) {
-//   if (modal == null) return
-//   modal.classList.add('active')
-//   overlay.classList.add('active')
+// ------------------------------------------------------------------
+// function open(e){
+//   console.log(e.target.id);
+//   document.getElementById('m-modal').classList.toggle('active');
+//   document.getElementById('overlay').classList.toggle('active');
+//   document.getElementById('overlay').addEventListener('click', close);
 // }
 //
-// function closeModal(modal) {
-//   if (modal == null) return
-//   modal.classList.remove('active')
-//   overlay.classList.remove('active')
+// function close(e){
+//   if(e.target.id == "overlay" || e.target.id == "close"){
+//   document.getElementById('m-modal').classList.toggle('active');
+//   document.getElementById('overlay').classList.toggle('active');
 // }
+// }
+// ----------------------------------------------------------------------------
