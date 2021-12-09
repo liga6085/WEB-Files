bf13ad784e2721c477cb33cd0a54586a85d11ab1
liga6085 <70085102+liga6085@users.noreply.github.com>
2021-12-09 13:34:37 -0700

    Update portfolio.js

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -42,5 +42,18 @@ for(let i = 0; i<list.length; i++){
       list[j].classList.remove('active');
     }
     this.classList.add('active');
+
+    let dataFilter = this.getAttribute('data-filter');
+
+    for( let k = 0; k<itemBox.length; k++){
+      itemBox[k].classList.remove('active');
+      itemBox[k].classList.add('hide');
+
+      if(itemBox[k].getAttribute('data-item') == dataFilter || dataFilter == "all"){
+        itemBox[k].classList.remove('hide');
+        itemBox[k].classList.add('active');
+      }
+    }
+
   })
 }
